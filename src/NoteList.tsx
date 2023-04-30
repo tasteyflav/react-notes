cb1c8b3e4218bc3d0106d9e1dac82899bcfcfc94
Mark Venema <markavenema@gmail.com>
2023-04-30 09:46:14 +0900

    1

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,55 +1,66 @@
-import { useState } from "react";
-import { Row, Col, Stack, Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import ReactSelect from "react-select";
-import { Tag } from ".App";
+import { useState } from 'react';
+import { Row, Col, Stack, Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import ReactSelect from 'react-select';
+import { Tag } from './App';
 
-export function NoteList({availableTags}) {
-    const [selectedTags, setSelectedTags] = useState<Tag[]>({})
-    return (
-        <>
-            <Row>
-                <Col><h1>Notes</h1></Col>
-                <Col xs="auto">
-                    <Stack gap={2} direction="horizontal">
-                        <Link to="/new">
-                            <Button variant="primary">Create</Button>
-                        </Link>
-                        <Button variant="outline-secondary">Edit Tags</Button>
-                    </Stack>
-                </Col>
-            </Row>
-            <Form>
-                <Row className="mb-4">
-                    <Col>
-                        <Form.Group controlId="title">
-                            <Form.Label>Title</Form.Label>
-                            <Form.Control type="text" />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                     <Form.Group controlId='tags'>
+type NoteListProps = {
+  availableTags: Tag[];
+};
+
+export function NoteList({ availableTags }: NoteListProps) {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [title, setTitle] = useState<string>('');
+  return (
+    <>
+      <Row className='align-items-center mb-4'>
+        <Col>
+          <h1>Notes</h1>
+        </Col>
+        <Col xs='auto'>
+          <Stack gap={2} direction='horizontal'>
+            <Link to='/new'>
+              <Button variant='primary'>Create</Button>
+            </Link>
+            <Button variant='outline-secondary'>Edit Tags</Button>
+          </Stack>
+        </Col>
+      </Row>
+      <Form>
+        <Row className='mb-4'>
+          <Col>
+            <Form.Group controlId='title'>
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type='text'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId='tags'>
               <Form.Label>Tags</Form.Label>
               <ReactSelect
-                
                 value={selectedTags.map((tag) => {
                   return { label: tag.label, value: tag.id };
                 })}
-                options={availableTags.map(tag => {
-                    return { label: tag.label, value: tag.id }
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
                 })}
-                onChange={tags => {
-                    setSelectedTags(
-                        tags.map(tag => {
-                        return { label: tag.label, id: tag.value };
-                    }));
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
                 }}
                 isMulti
               />
             </Form.Group>
-                    </Col>
-                </Row>
-            </Form>
-        </>
-    )
-} 
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+}
